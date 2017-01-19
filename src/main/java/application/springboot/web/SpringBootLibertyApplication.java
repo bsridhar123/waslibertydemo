f659420c73ee7eb889e0bae13d87dc8b4576caa6
Balasubramanian, Sridhar <sridhar.balasubramanian@anthem.com>
2017-01-19 17:39:55 +0530

    first commit

--- src/main/java/application/springboot/web/SpringBootLibertyApplication.java
+++ src/main/java/application/springboot/web/SpringBootLibertyApplication.java
@@ -19,10 +19,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.MessageSourceAutoConfiguration;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude = MessageSourceAutoConfiguration.class)
-@EnableEurekaClient
+//@EnableEurekaClient
 public class SpringBootLibertyApplication extends SpringBootServletInitializer {
 
     @Override
